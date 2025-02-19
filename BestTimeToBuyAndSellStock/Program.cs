3ff2dd97d996ebbb3f9871cb07b415dfc9a6a627
Duong Nguyen Hoang <hoangduongatwork@gmail.com>
2025-02-19 21:37:47 +0700

    init

--- BestTimeToBuyAndSellStock/Program.cs
+++ BestTimeToBuyAndSellStock/Program.cs
@@ -5,13 +5,19 @@
 // Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 // Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
-string[] input = Console.ReadLine()?.Split(" ") ?? throw new InvalidOperationException();
+string[] input = Console.ReadLine()?.Split(",") ?? throw new InvalidOperationException();
 int[] prices = Array.ConvertAll(input, int.Parse);
 
-int maxPrice = prices[0];
-int maxIndex = 0;
-int minIndex = 0;
-int minPrice = prices[0];
+int maxProfit = 0;
 for (int i = 0; i < prices.Length; i++)
 {
+    for (int j = i; j < prices.Length; j++)
+    {
+        if (prices[j] - prices[i] >= maxProfit)
+        {
+            maxProfit = prices[j] - prices[i];
+        }
+    }
 }
+
+Console.WriteLine(maxProfit);
